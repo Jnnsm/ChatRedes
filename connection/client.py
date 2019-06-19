9bfa54651f439a9ab3fd990d5dd64ce24bf92cfb
Eduardo <eduardo.j.moreira@ufv.br>
2019-06-19 20:49:57 -0300

    Modificações na main e no cliente

--- connection/client.py
+++ connection/client.py
@@ -6,17 +6,29 @@ class Client:
 
     def __init__(self, username, host_address, port=20000):
         self._username = username
-        self._host_address = host_address
+        self._host_info = (host_address, port)
 
-        u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        u.bind((host_address, port))
-        thread = threading.Thread(target=self.udp, args=(u,))
-        thread.start()
+        u_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        u_receive.bind(self._host_info)
 
-    def udp(self, u):
+        thread_receive = threading.Thread(target=self.udp_receive, args=(u_receive,))
+        thread_receive.start()
+
+        u_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        thread_send = threading.Thread(target=self.udp_send, args=(u_send,))
+        thread_send.start()
+
+    def udp_receive(self, u):
         while True:
             message, client = u.recvfrom(1024)
-            print(client,": ", message.decode())
+            print(client, ": ", message.decode())
+        u.close()
+
+    def udp_send(self, u):
+        message = "USER:" + self._username
+        while message != '/bye':
+            u.sendto(message.encode(), self._host_info)
+            message = input()
         u.close()
 
     def tcp(self):
