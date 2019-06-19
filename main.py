13685849c555fe9f88e6c7a4fe25222258081736
Eduardo <eduardo.j.moreira@ufv.br>
2019-06-19 20:04:06 -0300

    Adicionado argparse

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ Mateus Armond Santos 89399
 import argparse
 from connection import *
 
+
+def main():
+    parser = argparse.ArgumentParser(description='Main do programa para Cliente/Servidor da troca de mensagens')
+    parser.add_argument('type', help='Tipo de execução de programa, C para cliente e S para servidor')
+    parser.add_argument('username', required=False, help='Username do cliente')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
