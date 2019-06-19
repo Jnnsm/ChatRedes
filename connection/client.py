99c39e60e1c671c8cd1d2804362b44f30e5e21d7
Eduardo <eduardo.j.moreira@ufv.br>
2019-06-19 20:23:31 -0300

    Preparada a main e começando o client

--- connection/client.py
+++ connection/client.py
@@ -4,5 +4,20 @@ import socket
 
 class Client:
 
-    def __init__(self):
+    def __init__(self, username, host_address, port=20000):
+        self._username = username
+        self._host_address = host_address
+
+        u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        u.bind((host_address, port))
+        thread = threading.Thread(target=self.udp, args=(u,))
+        thread.start()
+
+    def udp(self, u):
+        while True:
+            message, client = u.recvfrom(1024)
+            print(client,": ", message.decode())
+        u.close()
+
+    def tcp(self):
         pass
--- main.py
+++ main.py
@@ -3,14 +3,25 @@ Eduardo Júnior Dias dos Santos Moreira 89395
 Mateus Armond Santos 89399
 """
 import argparse
-from connection import *
+from connection import server, client
 
 
 def main():
     parser = argparse.ArgumentParser(description='Main do programa para Cliente/Servidor da troca de mensagens')
     parser.add_argument('type', help='Tipo de execução de programa, C para cliente e S para servidor')
     parser.add_argument('username', required=False, help='Username do cliente')
+    parser.add_argument('host', required=False, help='Endereco do servidor')
     args = parser.parse_args()
+
+    conn = None
+    if args.type == "S" or args.type == "s":
+        conn = server.Server()
+    elif args.type == "C" or args.type == "c":
+        try:
+            conn = client.Client(args.username, args.host)
+        except Exception as exception:
+            print("Argumentos devem ser passados para conexão com o servidor!")
+            return
 
 
 if __name__ == '__main__':
