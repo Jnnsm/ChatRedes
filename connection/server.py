c6cb05eb67168efc6cda31ed50d263ffc2e1dcdc
matarms <mateus.armond97@gmail.com>
2019-06-19 23:44:14 -0300

    servidor com BYE

--- connection/server.py
+++ connection/server.py
@@ -5,11 +5,20 @@ import time
 
 class Server:
     udp = None
+    tcp = None
+    port = None
     clients = {}
+    keep_time = 0
     def __init__(self, host='', port=20000):
+        self.port = port
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         origem = (host, port)
         self.udp.bind(origem)
+
+        self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.bind(tcp.bind(orig))
+
+        self.keep_time = time.time()
 
         self.listen_udp()
     
@@ -17,12 +26,20 @@ class Server:
         while True:
             msg, cliente = self.udp.recvfrom(1024)
             
+            msg_decode = msg.decode()
             print(cliente, msg.decode())
-            if(cliente[0] in self.clients):
-                self.resend_msg(msg, cliente[0])
+
+            if(msg_decode == 'BYE'): #retira cliente da lista de conectados
+                    print("Cliente {} desconectou".format(self.clients[cliente[0]][0]))
+                    del self.clients[cliente[0]]
+            elif(msg_decode == 'LINK'):
+                for k in self.clients:
+                    self.send_msg(self.clients[k][0], cliente[0])
+            elif( 'FILE' == msg_decode[0:4] ):
+                pass
+            elif(cliente[0] in self.clients):
+                self.broadcast_exc(msg_decode, cliente[0], self.port)
             else:
-                
-                msg_decode = msg.decode()
                 split_msg = msg_decode.split(':')
                 
                 #Adiciona na lista de clientes
@@ -33,37 +50,35 @@ class Server:
 
                     self.send_msg("ACK", cliente[0]) #Envia ACK para o cliente conectado
 
-            
+            #Envia KEEP a cada 10 segundos
+            if time.time() - self.keep_time > 10:
+                self.broadcast("KEEP")
+        self.udp.close()
+    
 
     def send_msg(self, message, host, port=20000):
         '''Envia uma mensagem em forma de string'''
-        udp_msg = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         destino = (host, port)
-        udp_msg.sendto(message.encode(), destino)
-        udp_msg.close()
+        self.udp.sendto(message.encode(), destino)
 
     def resend_msg(self, message, host, port=20000):
         '''Envia uma mensagem em bytes mensagem em forma de string'''
-        udp_msg = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         destino = (host, port)
-        udp_msg.sendto(message, destino)
-        udp_msg.close()
+        self.udp.sendto(message, destino)
 
     def broadcast_exc(self, message, excluded_host, port=20000):
         '''Envia mensagem para todos os usuários menos quem mandou a mensagem e coloca a identificação'''
 
         #Junta mensagem com a identificação de quem mandou
-        new_message = "MSG:{}:".format(self.clients[excluded_host])
-        new_message = new_message.encode() + message
+        new_message = "MSG:{}:{}".format(self.clients[excluded_host], message)
 
         for hosts in self.clients: #Envia a mensagem para os clientes cadastrados
             if hosts != excluded_host:
-                self.resend_msg(new_message, hosts, port)
+                self.send_msg(new_message, hosts, port)
 
-    def broadcast(self, message, excluded_host, port=20000):
-        '''Envia mensagem para todos'''
+    def broadcast(self, message, port=20000):
+        '''Envia mensagem para todos os clientes cadastrados'''
 
-        for hosts in self.clients: #Envia a mensagem para os clientes cadastrados
-            if hosts != excluded_host:
-                self.resend_msg(message, hosts, port)
+        for hosts in self.clients:
+            self.send_msg(message, hosts, port)
             
