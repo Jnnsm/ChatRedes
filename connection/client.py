1c83b618b524b96f84437031eb03ac90d400b297
Eduardo <eduardo.j.moreira@ufv.br>
2019-06-20 01:00:22 -0300

    Talvez envia e recebe arquivos, o tempo dirá

--- connection/client.py
+++ connection/client.py
@@ -15,92 +15,135 @@ class Client:
         # É criado um socket único para caso a conexão seja fechada para escrita ela também será fechada para ouvir e o
         # programa se encerrará
 
-        u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        u.bind(('', port))
+        self._u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._u.bind(('', port))
+
+        self._t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._t.bind(('', port))
+        self._t.listen(1)
 
         # Variável que define se o usuário recebeu ou não um ack do servidor
         self.received_ack = False
 
         # Define a thread para receber mensagens
-        thread_receive = threading.Thread(target=self.udp_receive, args=(u,))
+        thread_receive = threading.Thread(target=self.udp_receive)
         thread_receive.start()
 
         # Define a primeira mensagem para o servidor é USER: + username
-        self.udp_send(u, "USER:" + self._username)
+        self.udp_send("USER:" + self._username)
 
         # Espera resposta do servidor antes de começar um chat, caso ele esteja offline encerra o programa
         start_time = time.time()
         while True:
             if time.time() - start_time > 10 and self.received_ack is False:
                 print('Servidor desconectado. Encerrando o programa')
-                u.close()
+                self._u.close()
                 exit()
             elif self.received_ack is True:
                 print('Servidor conectado para chat')
                 break
 
         # Define a thread para entrar no chat
-        thread_send = threading.Thread(target=self.chat, args=(u,))
+        thread_send = threading.Thread(target=self.chat)
         thread_send.start()
 
     """
     Função definida para receber mensagens na porta definida em __init__ e responder caso seja necessário
     """
 
-    def udp_receive(self, u):
+    def udp_receive(self):
         while True:
             try:
-                message, client = u.recvfrom(1024)
+                message, client = self._u.recvfrom(1024)
                 message = message.decode()
                 # Caso a mensagem recebida seja um 'KEEP' responderemos
                 if message == 'KEEP':
-                    self.udp_send(u, 'KEEP')
+                    self.udp_send('KEEP')
+                # Caso recebemos um ack, confirmamos por meio da variável booleana global received_ack
                 elif message == 'ACK':
                     self.received_ack = True
+                # Caso recebemos um info mostramos o conteúdo da informação na tela
+                elif "INFO:" in message:
+                    print(message[6:])
                 # Mostra a mensagem recebida pelo usuário
                 else:
                     print(message)
             except Exception as exception:
                 break
-        u.close()
+        self._u.close()
 
     """
     Define a função para entrar no chat
     """
 
-    def chat(self, u):
+    def chat(self):
         while True:
             # Recebe uma mensagem de entrada e caso não seja /bye ele envia para o servidor
             message = input()
             if message[0] == '/':
+                # Caso seja uma mensagem de finalização saimos do loop
                 if message == '/bye':
                     break
+                # Caso seja um pedido de lista envia a palavra chave LIST
                 elif message == '/list':
-                    self.udp_send(u, 'LIST')
+                    self.udp_send('LIST')
+                # Caso seja um get de um arquivo envia a palavra chave GET com o nome do arquivo logo em seguida, além
+                # de estabelecer uma conexão tcp para essa transferencia
                 elif '/get' in message:
-                    self.udp_send(u, 'GET' + message[4:])
+                    self.udp_send('GET' + message[4:])
+                    thread_tcp_receive = threading.Thread(target=self.tcp_receive, args=message[4:])
+                    thread_tcp_receive.start()
+                # Caso seja a inserção de um arquivo envia a palavra FILE e o nome do arquivo, além de estabelecer uma
+                # conexão tcp para essa transferencia
                 elif '/file' in message:
-                    self.udp_send(u, 'FILE' + message[5:])
+                    self.udp_send('FILE' + message[5:])
+                    thread_tcp_send = threading.Thread(target=self.tcp_send, args=message[5:])
+                    thread_tcp_send.start()
             else:
                 message = "MSG:" + message
-                self.udp_send(u, message)
+                self.udp_send(message)
 
         # Envia a mensagem final de que o cliente está saindo
-        self.udp_send(u, 'BYE')
-        u.close()
+        self.udp_send('BYE')
+        self._u.close()
 
     """
     Envia uma mensagem qualquer, por meio de uma conexão U para um target qualquer, o padrão é para o servidor conectado
     """
 
-    def udp_send(self, u, message, target=None):
+    def udp_send(self, message, target=None):
+
         # Se não tiver um target especifico mandar para o servidor
         if target is None:
             target = self._host_info
         try:
-            u.sendto(message.encode(), target)
+            self._u.sendto(message.encode(), target)
         except Exception as excepton:
             return
 
-    def tcp(self):
-        pass
+    def tcp_receive(self, filename):
+        data = ""
+        try:
+            connection, sender = self._t.accept()
+            while True:
+                message = connection.recv(1024).decode()
+                if message == 'BYE':
+                    break
+                data += message
+            connection.close()
+            file = open(filename, "w")
+            file.write(data)
+            file.close()
+        except Exception as ex:
+            return
+
+    def tcp_send(self, filename):
+        self._t.connect(self._host_info)
+        file = open(filename, "r")
+
+        data = file.read(32)
+        while data != "":
+            self._t.send(data.encode())
+        self._t.send('BYE'.encode())
+
+        file.close()
