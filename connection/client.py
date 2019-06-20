db63f839267504d8203fca2aefff52da1033068e
Eduardo <eduardo.j.moreira@ufv.br>
2019-06-19 22:26:21 -0300

    Cliente que ouve, envia e keep implementado. Também comentado

--- connection/client.py
+++ connection/client.py
@@ -4,34 +4,71 @@ import socket
 
 class Client:
 
+    """
+    Função definida para iniciar as conexões e guardar o username e o endereço e porta do host
+    """
     def __init__(self, username, host_address, port=20000):
         self._username = username
         self._host_info = (host_address, port)
 
-        u_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        u_receive.bind(self._host_info)
+        # É criado um socket único para caso a conexão seja fechada para escrita ela também será fechada para ouvir e o
+        # programa se encerrará
 
-        thread_receive = threading.Thread(target=self.udp_receive, args=(u_receive,))
+        u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        u.bind(('', port))
+
+        # Define a thread para receber mensagens
+        thread_receive = threading.Thread(target=self.udp_receive, args=(u,))
         thread_receive.start()
 
-        u_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        thread_send = threading.Thread(target=self.udp_send, args=(u_send,))
+        # Define a thread para entrar no chat
+        thread_send = threading.Thread(target=self.chat, args=(u,))
         thread_send.start()
 
+    """
+    Função definida para receber mensagens na porta definida em __init__ e responder caso seja necessário
+    """
     def udp_receive(self, u):
         while True:
-            message, client = u.recvfrom(1024)
-            print(message.decode())
+            try:
+                message, client = u.recvfrom(1024)
+                # Caso a mensagem recebida seja um 'KEEP' responderemos
+                if message.decode() == 'KEEP':
+                    self.udp_send(u, 'KEEP')
+                # Mostra a mensagem recebida pelo usuário
+                print(message.decode())
+            except Exception as exception:
+                break
         u.close()
 
-    def udp_send(self, u, target=None):
+    """
+    Define a função para entrar no chat
+    """
+    def chat(self, u):
+        # Define a primeira mensagem para o servidor é USER: + username
+        self.udp_send(u, "USER:" + self._username)
+
+        while True:
+            # Recebe uma mensagem de entrada e caso não seja /bye ele envia para o servidor
+            message = input()
+            if message == '/bye':
+                break
+            message = "MSG:" + message
+            self.udp_send(u, message)
+
+        # Envia a mensagem final de que o cliente está saindo
+        u.sendto('BYE'.encode(), self._host_info)
+        u.close()
+
+    """
+    Envia uma mensagem qualquer, por meio de uma conexão U para um target qualquer, o padrão é para o servidor conectado
+    """
+    def udp_send(self, u, message, target=None):
+        # Se não tiver um target especifico mandar para o servidor
         if target is None:
             target = self._host_info
-        message = "USER:" + self._username
-        while message != '/bye':
-            u.sendto(message.encode(), target)
-            message = "MSG:" + input()
-        u.close()
+
+        u.sendto(message.encode(), target)
 
     def tcp(self):
         pass
